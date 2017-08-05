bdcd4c0f7c45577c62d94e06134e5d68d3a37a9c
Richard Jean-Baptiste <Richinbk1@gmail.com>
2017-08-05 08:45:07 -0400

    added icons and imgs that wont break overtime

--- app.js
+++ app.js
@@ -6,16 +6,15 @@ $(document).ready(function() {
   var celsius;
 
       
-
-   function changeBackground(fTemp){
+     function changeBackground(fTemp){
           if (fTemp > 80) {
-            $("body").css("background-image", "url(http://www.whiskyisrael.co.il/wp-content/uploads/2016/07/summertime-7-super-strategies-for-seniors.jpg)");
+            $("body").addClass("summer");
           } else if (fTemp > 60) {
-            $("body").css("background-image", 'url(http://az616578.vo.msecnd.net/files/2016/08/11/636065554421332270-922755664_fall%20(1).jpg)');
+            $("body").addClass("spring");
           } else if (fTemp > 40) {
-            $("body").css('background-image', 'url(https://www.stgeorges.co.uk/sites/default/files/blog_uploads/2012/02/snow_london.jpg)');
+            $("body").addClass("winter");
           } else if (fTemp > 100) {
-            $("body").css('background-image', 'url(http://rappingmanual.com/wp-content/uploads/2014/10/mixtape3.jpg)');
+            $("body").addClass("blazing");
           }
 
           console.log("background changed");
@@ -38,8 +37,10 @@ $(document).ready(function() {
         console.log(kelvin);
         var windSpeed = data.wind.speed;
         var city = data.name;
+        var iconCode = data.weather[0].icon;
         fTemp = ((kelvin) * (9 / 5) - 459.67).toFixed(2);
         celsius = (kelvin - 273).toFixed(2);
+        
         var tempSwap = true;
          $("#temp").click(function() {
           if (tempSwap === false) {
@@ -49,16 +50,14 @@ $(document).ready(function() {
             $("#temp").html(celsius + "&#8451;");
             tempSwap = false;
           }
-        
-     });
-        $("#city").html(city);
-        $("#weatherType").html(weatherType);
-        $("#temp").html(fTemp + "&#8457;");
-        $("body").css(changeBackground(fTemp));
-        
-      
+      });
 
-    });
+        $("#city").html(city);
+        $("#weatherType").html(weatherType + " " + "<img src=" + "http://openweathermap.org/img/w/" + iconCode + ".png" + " " + "style=" + "width:20px" + ">");
+        $("#temp").html(fTemp + "&#8457;");
+        $("body").addClass(changeBackground(fTemp));
+        
+      });
   });
 }
 
